6e764bc15cffd424a3a0e312a6bb1ad821cddd37
ColinRCasto <Colin@wildheartinteractive.com>
2021-11-27 01:53:01 -0500

    added walkspeeds field to player movement

--- src/movement/player_movement.rs
+++ src/movement/player_movement.rs
@@ -2,6 +2,9 @@ pub use crate::prelude::*;
 pub struct PlayerMovement {
     grounded_dash: Option<Box<dyn Dash>>,
     grounded_backdash: Option<Box<dyn BackDash>>,
+    pub is_grounded: bool,
+    walk_speed: f32,
+    back_walk_speed: f32,
   }
   
   impl PlayerMovement {
@@ -9,6 +12,9 @@ pub struct PlayerMovement {
       PlayerMovement{
         grounded_dash: Some(Box::new(grounded_dash)),
         grounded_backdash: Some(Box::new(grounded_backdash)),
+        is_grounded: true,
+        walk_speed: 50.0,
+        back_walk_speed: 25.0
       }
     }
   }
