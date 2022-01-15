fef7dcc1bc0b26877a58d721e0d78448cf6076f2
ColinRCasto <Colin@wildheartinteractive.com>
2022-01-15 03:30:08 -0500

    implement checks for if hitboxes will interract, hit or be blocked

--- src/collision/hitbox.rs
+++ src/collision/hitbox.rs
@@ -3,17 +3,19 @@ pub use crate::prelude::*;
 /// Attack hitboxes
 pub struct Hitbox {
   pub player_id: u8,
-  attack_data: AttackData,
-  property: AttackProperty,
+  pub air_blockable: bool,
+  pub property: AttackProperty,
+  hitbox_data: HitboxData,
   duration: u8,
   active: bool
 }
 
 impl Hitbox {
-  pub fn new(player_id: u8, attack_data: AttackData, property: AttackProperty, duration: u8) -> Self {
+  pub fn new(player_id: u8, air_blockable: bool, hitbox_data: HitboxData, property: AttackProperty, duration: u8) -> Self {
     Hitbox {
       player_id,
-      attack_data,
+      air_blockable,
+      hitbox_data,
       property,
       duration,
       active: true,
@@ -25,16 +27,52 @@ impl Hitbox {
   }
 
   ///
-  pub fn collide_with_hurtbox(&self,hurtbox: Hurtbox, mut hit_writer: EventWriter<HitEvent>) {
-    if hurtbox.does_connect(&self.property) {
-      hit_writer.send(HitEvent::new(self.attack_data, self.player_id, hurtbox.player_id));
+  pub fn collide_with_hurtbox(&self,hurtbox: &Hurtbox, mut hit_writer: EventWriter<HitEvent>, mut block_writer: EventWriter<BlockEvent>) {
+    if self.player_id != hurtbox.player_id {
+      if hurtbox.does_connect(&self.property) {
+        if self.is_blocked(hurtbox) {
+          block_writer.send(BlockEvent::new(self.hitbox_data, self.player_id, hurtbox.player_id))
+        } else {
+          hit_writer.send(HitEvent::new(self.hitbox_data, self.player_id, hurtbox.player_id));
+        }
+      }
+    }
+  }
+
+    pub fn is_blocked(&self, hurtbox: &Hurtbox) -> bool {
+    if let Some(block_type) = hurtbox.block_type {
+      if hurtbox.is_grounded {
+        match self.property {
+          AttackProperty::MID => {return true},
+          AttackProperty::HIGH => {
+            return block_type == BlockType::HIGH;
+          },
+          AttackProperty::LOW => {
+            return block_type == BlockType::LOW;
+          }
+        }
+      } else {
+        if self.air_blockable {
+          return true;
+        } else {
+          if let Some(modifier) = hurtbox.block_modifier {
+            match modifier {
+              _ => {return true}
+            }
+          } else {
+            return false;
+          }
+        }
+      }
+    } else {
+      return false;
     }
   }
 }
 
 /// All pertinent data for attacks
 #[derive(Clone, Copy)]
-pub struct AttackData {
+pub struct HitboxData {
   attack_level: u8,
   damage: u8,
   bonus_hitstun: u8,
@@ -42,15 +80,15 @@ pub struct AttackData {
   mid_proration: f32,
   i_force: InterpolatedForce,
   hit_state: HitState,
-  block_state: BlockState
+  block_state: BlockState,
 }
 
 /// Which part of the body does this attack hit?
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Copy)]
 pub enum AttackProperty {
-  HEAD,
-  BODY,
-  FOOT
+  HIGH,
+  MID,
+  LOW
 }
 
 #[derive(PartialEq, Clone, Copy)]
@@ -69,17 +107,35 @@ pub enum BlockState {
 }
 
 pub struct HitEvent{
-  pub attack_data: AttackData,
+  pub hitbox_data: HitboxData,
   pub player_id: u8,
   pub recieving_player_id: u8,
 }
 
 impl HitEvent {
-  pub fn new(attack_data: AttackData, player_id: u8, recieving_player_id: u8) -> Self {
+  pub fn new(hitbox_data: HitboxData, player_id: u8, recieving_player_id: u8) -> Self {
     HitEvent {
-      attack_data,
+      hitbox_data,
       player_id,
       recieving_player_id
     }
   }
 }
+
+pub struct BlockEvent{
+  pub hitbox_data: HitboxData,
+  pub player_id: u8,
+  pub recieving_player_id: u8,
+}
+
+impl BlockEvent {
+  pub fn new(hitbox_data: HitboxData, player_id: u8, recieving_player_id: u8) -> Self {
+    BlockEvent {
+      hitbox_data,
+      player_id,
+      recieving_player_id
+    }
+  }
+}
+
+
--- src/collision/hurtbox.rs
+++ src/collision/hurtbox.rs
@@ -1,8 +1,12 @@
 pub use crate::prelude::*;
 
+
 /// Player hurtboxes
 pub struct Hurtbox {
   pub player_id: u8,
+  pub is_grounded: bool,
+  pub block_type: Option<BlockType>,
+  pub block_modifier: Option<BlockModifier>,
   ignored_properties: Vec<AttackProperty>,
 }
 
@@ -11,3 +15,18 @@ impl Hurtbox {
     return !self.ignored_properties.contains(attack_property);
   }
 }
+
+#[derive(PartialEq, Clone, Copy)]
+pub enum BlockType {
+  HIGH,
+  LOW,
+}
+
+#[derive(PartialEq, Clone, Copy)]
+pub enum BlockModifier {
+  BARRIER,
+  INSTANT,
+}
+
+
+
